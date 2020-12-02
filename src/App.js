8742c9298ccc2a25bb551ca5349c870e4ad8e35e
subhampreet <subhampreet18@gmail.com>
2020-12-02 23:36:31 +0530

    Login Page Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Music App 💖</h1>        
+        <Login />
     </div>
   );
 }
